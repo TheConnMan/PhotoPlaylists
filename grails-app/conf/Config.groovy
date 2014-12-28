14a6cd3aa084b85402ab36d1fc389abd693e01aa
TheConnMan <bcconn2112@gmail.com>
2014-12-28 15:52:03 -0500

    Added devdeploy environment.

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -87,6 +87,10 @@ environments {
     development {
         grails.logging.jul.usebridge = true
     }
+    devdeploy {
+        grails.logging.jul.usebridge = false
+        // TODO: grails.serverURL = "http://www.changeme.com"
+    }
     production {
         grails.logging.jul.usebridge = false
         // TODO: grails.serverURL = "http://www.changeme.com"
--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -26,6 +26,41 @@ environments {
             url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
         }
     }
+    devdeploy {
+        dataSource {
+            dbCreate = "create-drop"
+            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+            properties {
+               // Documentation for Tomcat JDBC Pool
+               // http://tomcat.apache.org/tomcat-7.0-doc/jdbc-pool.html#Common_Attributes
+               // https://tomcat.apache.org/tomcat-7.0-doc/api/org/apache/tomcat/jdbc/pool/PoolConfiguration.html
+               jmxEnabled = true
+               initialSize = 5
+               maxActive = 50
+               minIdle = 5
+               maxIdle = 25
+               maxWait = 10000
+               maxAge = 10 * 60000
+               timeBetweenEvictionRunsMillis = 5000
+               minEvictableIdleTimeMillis = 60000
+               validationQuery = "SELECT 1"
+               validationQueryTimeout = 3
+               validationInterval = 15000
+               testOnBorrow = true
+               testWhileIdle = true
+               testOnReturn = false
+               ignoreExceptionOnPreLoad = true
+               // http://tomcat.apache.org/tomcat-7.0-doc/jdbc-pool.html#JDBC_interceptors
+               jdbcInterceptors = "ConnectionState;StatementCache(max=200)"
+               defaultTransactionIsolation = java.sql.Connection.TRANSACTION_READ_COMMITTED // safe default
+               // controls for leaked connections 
+               abandonWhenPercentageFull = 100 // settings are active only when pool is full
+               removeAbandonedTimeout = 120000
+               removeAbandoned = true
+               // use JMX console to change this setting at runtime
+               logAbandoned = false // causes stacktrace recording overhead, use only for debugging
+            }
+        }
     production {
         dataSource {
             dbCreate = "update"
