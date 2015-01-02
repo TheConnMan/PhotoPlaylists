3c82c3cd5099ffefeb5d87e2abc21fa56b502859
TheConnMan <bcconn2112@gmail.com>
2015-01-01 19:49:22 -0500

    Added Create Playlist action. Ref #4.

--- grails-app/controllers/com/madconn/photoplaylists/PlaylistController.groovy
+++ grails-app/controllers/com/madconn/photoplaylists/PlaylistController.groovy
@@ -5,6 +5,7 @@ import static org.springframework.http.HttpStatus.*
 import java.util.Map;
 
 import grails.transaction.Transactional
+import grails.converters.JSON
 import grails.plugin.springsecurity.annotation.Secured
 
 @Transactional(readOnly = true)
@@ -14,19 +15,33 @@ class PlaylistController {
 
 	def scaffold = true
 	
-	@Secured(["ROLE_USER", "ROLE_ADMIN"])
 	def home() {
 		[menu: menu()]
 	}
 	
-	@Secured(["ROLE_USER", "ROLE_ADMIN"])
 	def view(Long id) {
 		Playlist playlist = Playlist.get(id);
 		[menu: menu(), playlist: playlist]
 		
 	}
 	
-	@Secured(["ROLE_USER", "ROLE_ADMIN"])
+	def createPlaylist() {
+		Playlist playlist = new Playlist(
+			name: params.name,
+			description: params.description,
+			createdBy: springSecurityService.currentUser,
+			createdDate: new Date(),
+			lastViewedDate: new Date(),
+			lastEditedDate: new Date()
+		);
+		playlist.save();
+		if (playlist.hasErrors()) {
+			render([false: true] as JSON)
+		} else {
+			render([success: true, id: playlist.id] as JSON)
+		}
+	}
+	
 	Map menu() {
 		[playlists: Playlist.findAllByCreatedBy(springSecurityService.currentUser)]
 	}
